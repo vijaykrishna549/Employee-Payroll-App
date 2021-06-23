c5a329c82e1656ce8ac4854e5a1998cb02289de2
pvijaykrishna549 <you@example.com>
2021-06-23 17:47:25 +0530

    To Validate Name, Set Event To Salary And Populate Employee Parroll Data

---
+++ js/Payroll_Form.js
@@ -0,0 +1,97 @@
+class EmployeePayrollData {
+    
+    //Getters and Setters
+    get id(){
+        return this._id;
+    }
+    set id(id){
+        this._id = id;
+    }
+
+    
+    get name() {return this._name}
+    set name(name) {
+        let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$')
+        if(nameRegex.test(name))
+        this._name = name;
+        else throw 'Name is Incorrect';
+    }
+
+    get profilePic(){
+        return this._profilePic;
+    }
+    set profilePic(profilePic){
+        this._profilePic = profilePic;
+    }
+
+    get gender(){
+        return this._gender;
+    }
+    set gender(gender){
+        this._gender = gender;
+    }
+
+    get department() {
+        return this._department
+    }
+    set department(department) {
+        this._department = department
+    }
+
+    get salary(){
+        return this._salary;
+    }
+    set salary(salary){
+        this._salary = salary;
+    }
+
+    get note(){
+        return this._note;
+    }
+    set note(note){
+        this._note = note;
+    }
+
+    get startDate(){
+        return this._startDate;
+    }
+    set startDate(startDate){
+        this._startDate = startDate;
+    }
+
+    //method
+    toString() {
+        const options ={ year: 'numeric', month: 'long', day:'numeric'};
+        const empDate = !this.startDate ? "undefined":
+                         this.startDate.toLocaleDateString("en-US", options);
+    return "id=" +this.id + ", Name=" + this.name +", Gender=" + this.gender 
+    +", ProfilePic= " + this.profilePic + ",Department=" + this.department 
+    + ", Salary=" + this.salary + ", StartDate=" + this.empDate + ", Note=" + this.note ;                     
+    
+}
+
+}
+window.addEventListener('DOMContentLoaded', (event) => {
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error')
+    name.addEventListener('input', function() {
+        if(name.value.length ==0){
+            textError.textContent = "";
+            return;
+        }
+        try{
+            (new EmployeePayrollData()).name = name.value;;
+            textError.textContent =""; 
+        }
+        catch(e) {
+            textError.textContent = e;
+        }
+    });
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector('.salary-output');
+    output.textContent = salary.value;
+    salary.addEventListener('input', function() {
+        output.textContent = salary.value;
+    });
+})
+
